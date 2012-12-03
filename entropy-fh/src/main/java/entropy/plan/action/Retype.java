9bd3cd8a51dab763530ba7795da403c93f5d01ef
homer <mathieu.root@gmail.com>
2012-12-03 17:53:32 +0100

    Bits of code for the special case when the new type of a node is already known. Retype.java sequentially put a node offline, change its platform and put it back online. RetypeNodeActionModel.java creates two slices (c & d) for the retyping operation; it also creates a cSlice for each virtual machines running on the node. each one must have an end time inferior to the action's cslice.end(). it uses one action (Retype).
    questions/remarks: where to implements constraints on dslices for the
    incoming vms?
    need to add fence for the « old » VMs so that they move
    elsewhere (or use Offline on the node?)
    
    need to add a parameter indicating the new type.

---
+++ entropy-fh/src/main/java/entropy/plan/action/Retype.java
@@ -0,0 +1,126 @@
+package entropy.plan.action;
+
+import entropy.configuration.Configuration;
+import entropy.configuration.Node;
+import entropy.execution.TimedExecutionGraph;
+import entropy.plan.parser.TimedReconfigurationPlanSerializer;
+import entropy.plan.visualization.PlanVisualizer;
+
+public class Retype extends NodeAction {
+	String newPlatform = "";
+
+	/**
+	 * Create a new time-unbounded retype action on an offline node.
+	 *
+	 * @param n The node to start
+	 */
+	public Retype(Node n) {
+		this(n, 0, 0);
+	}
+
+	/**
+	 * Create a new time-bounded retype action on an offline node.
+	 *
+	 * @param n The node to start
+	 * @param s the moment the action starts
+	 * @param f the moment the action is finished
+	 */
+	public Retype(Node n, int s, int f) {
+		super(n, s, f);
+	}
+
+	/**
+	 * Test the equality with another object.
+	 *
+	 * @param obj The object to compare with
+	 * @return true if o is an instance of Retype and if both actions act on the same node
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		} else if (obj == this) {
+			return true;
+		} else if (obj.getClass() == this.getClass()) {
+			return this.getNode().equals(((Retype) obj).getNode());
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return this.getNode().hashCode();
+	}
+
+	/**
+	 * Textual representation of the retype action.
+	 *
+	 * @return a String
+	 */
+	@Override
+	public String toString() {
+		StringBuilder buffer = new StringBuilder("retype(");
+		buffer.append(this.getNode().getName());
+		buffer.append(")");
+		return buffer.toString();
+	}
+
+	/**
+	 * XXX
+	 *
+	 * @param c the configuration
+	 */
+	@Override
+	public boolean apply(Configuration c) {
+		c.addOffline(node);
+		node.setCurrentPlatform(newPlatform);
+		return true;
+	}
+
+	/**
+	 * XXX
+	 *
+	 * @param src the configuration to check
+	 * @return {@code true}
+	 */
+	@Override
+	public boolean isCompatibleWith(Configuration src) {
+		return true;
+	}
+
+
+	/**
+	 * XXX
+	 * 
+	 *
+	 * @param src the source configuration
+	 * @param dst the configuration to reach
+	 * @return true 
+	 */
+	@Override
+	public boolean isCompatibleWith(Configuration src, Configuration dst) {
+		return true;
+	}
+
+	/**
+	 * Insert the action as an outgoing action. In creates resources!
+	 *
+	 * @param g the graph to use
+	 * @return true if the insertion succeed
+	 */
+	@Override
+	public boolean insertIntoGraph(TimedExecutionGraph g) {
+		return g.getUnlockings(this.getNode()).add(this);
+	}
+
+	@Override
+	public void injectToVisualizer(PlanVisualizer vis) {
+		vis.inject(this);
+	}
+
+	@Override
+	public void serialize(TimedReconfigurationPlanSerializer s) throws IOException {
+		s.serialize(this);
+	}
+
+}
