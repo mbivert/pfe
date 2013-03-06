42f0ade11f5b76f1577922c627c19f7ab03ccb00
homer <mathieu.root@gmail.com>
2013-03-06 17:28:01 +0100

    final commit

---
+++ entropy-fh/src/test/java/entropy/vjob/constraint/TestTypedPlatform.java
@@ -0,0 +1,11 @@
+package entropy.vjob.constraint;
+
+import org.testng.annotations.Test;
+
+
+@Test(groups = {"unit"})
+public class TestTypedPlatform {
+    public void testIsSatisfied() {
+
+    }
+}
